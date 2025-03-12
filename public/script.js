bd1530803383c457c5a869666867bda89b5d9806
Craig.Williams <craigjmwilliams@gmail.com>
2025-03-11 23:34:54 -0400

    more changes made

---
+++ public/script.js
@@ -0,0 +1,18 @@
+myobj = {name: "John",age: 31,city: "New York"};
+
+let obj2 = myobj;
+
+//obj2 and myobj are the same variable
+
+obj2.name = "Shelly"; // esentially copies obj1
+
+console.log(myobj.name);//Shelly
+
+//if you need to create a new object and copy its values use Object.assign
+
+let obj3 = {};
+Object.assign(obj3, myobj);
+
+obj3.name = 'Smith';
+
+console.log(myobj.name, obj3.name);//Shelly, Smith
